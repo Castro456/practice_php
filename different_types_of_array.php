b0d451e023da952da7db6a89dce96a549af11d1f
Castro456 <castrosid456@gmail.com>
2021-06-18 02:03:21 +0530

    multidimentional arrays

--- different_types_of_array.php
+++ different_types_of_array.php
@@ -19,9 +19,16 @@ $anotherdata["age"] = 20;
 print_r($anotherdata); //This prints all the value Array([name] => Zac [age] => 20)
 
 // Multidimentional Array
-$data = array(
+$multidata = array(  //can also get array values in array like $multidata[1] or any index names can be given
   array(
-    "name",
-    "age"
+    "name" => "Hikki",
+    "age" => 17
   ), "Hide" , 21
 );
+echo '<br>';
+print_r($multidata[0]['name']); //to print array within an array now it prints Hikki
+echo '<br>';
+
+foreach ($multidata as $row) { //$row prints only elements not index, so in here hikki and 21 is becomes just like  values so there is no way of calling thoes values, for array name and age can be only called if in foreach $multidata[0] is mentioned
+  print_r($row);
+}
